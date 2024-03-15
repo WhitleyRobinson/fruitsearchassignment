8cadd23e66c12eef094bf3b3e0060295543561e2
WhitleyRobinson <159495983+WhitleyRobinson@users.noreply.github.com>
2024-03-15 09:04:32 -0500

    Add files via upload

---
+++ Fruit Search Assignment/script.js
@@ -0,0 +1,37 @@
+const input = document.querySelector('#fruit');
+const suggestions = document.querySelector('.suggestions');
+
+const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+function search(str) {
+	let results = fruit.filter((f) => {
+		return f.toLowerCase().includes(str.toLowerCase());
+	});
+	showSuggestions(results, str);
+};
+	// TODO
+function searchHandler(e) {
+	search(input.value);
+	// TODO
+}
+
+function showSuggestions(results, inputVal) {
+	const listed = results.map((result) => {
+		if(inputVal !== '') {
+			return "<li>" + result + "</li>";
+		}
+	});
+	suggestions.innerHTML = "<ul>" + listed.join('') + "</ul>";
+};
+	// TODO
+
+
+function useSuggestion(e) {
+	input.value = e.target.innerText;
+	suggestions.innerHTML = '';
+}
+	// TODO
+
+
+input.addEventListener('keyup', searchHandler);
+suggestions.addEventListener('mouseup', useSuggestion);
